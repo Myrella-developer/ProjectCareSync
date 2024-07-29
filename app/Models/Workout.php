5238bfabc5c54bf9576570ab1b78d43e4c1b675a
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-07-29 13:37:02 +0200

    creaccion de tabla Workout y su vista provisional

---
+++ app/Models/Workout.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Workout extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'durationTime',
+    ];
+
+    /**
+     * Get the user that owns the Workout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
